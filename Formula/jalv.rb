99d538f328efc2d3b40738cf7080b41119221650
genevera <genevera@users.noreply.github.com>
2021-04-16 15:29:44 -0400

    update (#5)
    * ir.lv2: release 1.3.4
    
    * ir.lv2: add missing zita-convolver dependency
    
    * travis: disable zita-resolver test since it will be installed as an
    dependency of ir.lv2
    
    * add talentedhack.rb
    
    * add new formula talentedhack
    
    * jalv: verison 1.6.6
    
    * jalv: Fix crash when running jalv without arguments
    
    * Drop mrswatson: 404, not maintained anymore
    
    Co-authored-by: David <ottodavid@gmx.net>

--- Formula/jalv.rb
+++ Formula/jalv.rb
@@ -1,19 +1,29 @@
 class Jalv < Formula
-  desc "simple but fully featured LV2 host for Jack"
+  desc "Simple but fully featured LV2 host for Jack"
   homepage "http://drobilla.net/software/jalv/"
+  url "http://download.drobilla.net/jalv-1.6.6.tar.bz2"
+  sha256 "92d141781b664373207c343cebc5e9b8ced461faf26fdccb95df0007b0639e16"
   head "http://git.drobilla.net/jalv.git"
 
   depends_on "pkg-config" => :build
-  depends_on "lv2"
+  depends_on "gtk+"
   depends_on "jack"
   depends_on "lilv"
+  depends_on "lv2"
+  depends_on "qt@5"
   depends_on "serd"
   depends_on "sord"
-  depends_on "suil"
   depends_on "sratom"
-  depends_on "gtk+"
+  depends_on "suil"
   depends_on "gtk+3" => :optional
-  depends_on "qt5"
+
+  unless build.head?
+    # Fix crash when running jalv without arguments
+    patch do
+      url "https://gitlab.com/drobilla/jalv/-/commit/8952dde02d9d6761ae0cae033600f4a220c8e075.patch"
+      sha256 "5fad49ccdfcdd6f0fdb9d6005a64965e3ef3b7afa354ea0cc9b0858279400d50"
+    end
+  end
 
   # Workaround: Deadlock on program start, disable semaphores
   patch :p1, :DATA
